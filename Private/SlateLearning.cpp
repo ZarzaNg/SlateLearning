9aee8999dbd71af8619dc5052f2d52d8e7344373
ZarzaNg <45487646+ZarzaNg@users.noreply.github.com>
2024-04-23 08:53:46 +0800

    Expose the pointer.

--- Private/SlateLearning.cpp
+++ Private/SlateLearning.cpp
@@ -18,13 +18,40 @@ INT32_MAIN_INT32_ARGC_TCHAR_ARGV()
 	FSlateApplication::InitializeAsStandaloneApplication(GetStandardStandaloneRenderer());
 	FSlateApplication::InitHighDPI(true);
 
+	SOverlay::FOverlaySlot* Slot;
+	SHorizontalBox::FSlot* BoxPtr;
+
 	const TSharedPtr<SWindow> MainWindow = SNew(SWindow).ClientSize(FVector2D(800, 600))
 	[
-		SNew(SHorizontalBox) + SHorizontalBox::Slot().VAlign(EVerticalAlignment::VAlign_Top).HAlign(EHorizontalAlignment::HAlign_Left)
+		SNew(SOverlay) + SOverlay::Slot().VAlign(EVerticalAlignment::VAlign_Top).HAlign(EHorizontalAlignment::HAlign_Left).Expose(Slot)
 		[
-			SNew(SButton).Text(NSLOCTEXT("L10N", "Key", "Button Content"))
+			SNew(SHorizontalBox) + SHorizontalBox::Slot().AutoWidth().Expose(BoxPtr)
+			[
+				SNew(SButton).Text(NSLOCTEXT("L10N", "Key", "Add a button")).OnClicked_Lambda([=]()
+				{
+					SWidget& SlotWidget = Slot->GetWidget().Get();
+					SHorizontalBox& HorizontalBox = static_cast<SHorizontalBox&>(SlotWidget);
+					HorizontalBox.AddSlot()
+					[
+						SNew(SButton).Text(NSLOCTEXT("L10N", "Key", "Button Content"))
+					];
+					return FReply::Handled();
+				})
+			]
 		]
 	];
+
+	SWidget& SlotWidget = Slot->GetWidget().Get();
+	SHorizontalBox& HorizontalBox = static_cast<SHorizontalBox&>(SlotWidget);
+	// Failed to get the pointer in this way.
+	// SHorizontalBox& HorizontalBox = static_cast<SHorizontalBox&>(BoxPtr->GetWidget().Get());
+	for(uint8 i = 0; i < 5; i++)
+	{
+		HorizontalBox.AddSlot()
+		[
+			SNew(SButton).Text(NSLOCTEXT("L10N", "Key", "Button Content"))
+		];
+	}
 
 	FSlateApplication::Get().AddWindow(MainWindow.ToSharedRef());
 
